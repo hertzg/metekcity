cfc1e38791ac2a1b8f430e469e40875a4f14f7b3
George Hertz <georgedot@gmail.com>
2020-10-11 23:56:46 +0200

    ci: introduce eslint

---
+++ packages/esn00-packet/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+};
