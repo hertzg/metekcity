6c369624f9c0f53e683c3a6e5bbf5c2a26711ebf
George Hertz <georgedot@gmail.com>
2022-02-17 00:38:54 +0100

    feat(generic): uint16 to ArrayBuffer

--- packages/esn00-packet/src/lib/payloads/genericUInt16.ts
+++ packages/esn00-packet/src/lib/payloads/genericUInt16.ts
@@ -1,14 +1,15 @@
-import { bufferReadNumber, bufferWriteNumber } from '../utilities';
+import { arrayBufferReadNumber, arrayBufferWriteNumber } from '../utilities';
 import { IPayloadParselizer } from './index';
 
 export class UInt16Parselizer implements IPayloadParselizer {
   private readonly offset: number = 0;
 
-  parse = (buffer: Buffer): number => bufferReadNumber(buffer, this.offset);
+  parse = (buffer: ArrayBufferLike): number =>
+    arrayBufferReadNumber(buffer, this.offset);
 
-  serialize = (payload: number): Buffer => {
-    const buffer: Buffer = Buffer.alloc(2 + this.offset);
-    bufferWriteNumber(buffer, payload, this.offset);
+  serialize = (payload: number): ArrayBufferLike => {
+    const buffer = new ArrayBuffer(2 + this.offset);
+    arrayBufferWriteNumber(buffer, payload, this.offset);
     return buffer;
   };
 }
