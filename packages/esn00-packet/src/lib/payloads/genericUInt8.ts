bf72ba9bebedede3ee9b404521a9d74618435069
George Hertz <georgedot@gmail.com>
2022-02-17 00:38:58 +0100

    feat(generic): convert uint8 parselizer to ArrayBuffer

--- packages/esn00-packet/src/lib/payloads/genericUInt8.ts
+++ packages/esn00-packet/src/lib/payloads/genericUInt8.ts
@@ -1,13 +1,15 @@
 import { IPayloadParselizer } from './index';
+import { asDataView } from '../utilities';
 
 export class UInt8Parselizer implements IPayloadParselizer {
   private readonly offset: number = 0;
 
-  parse = (buffer: Buffer): number => buffer.readUInt8(this.offset);
+  parse = (buffer: ArrayBufferLike): number =>
+    asDataView(buffer).getUint8(this.offset);
 
-  serialize = (payload: number): Buffer => {
-    const buffer: Buffer = Buffer.alloc(1 + this.offset);
-    buffer.writeUInt8(payload, this.offset);
-    return buffer;
+  serialize = (payload: number): ArrayBufferLike => {
+    const dataView = asDataView(new Uint8Array(1 + this.offset));
+    dataView.setUint8(this.offset, payload);
+    return dataView.buffer;
   };
 }
