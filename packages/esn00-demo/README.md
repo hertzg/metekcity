b98441e8d456cec25462a9b1fe57ad2574fde6c9
George Hertz <georgedot@gmail.com>
2022-02-16 20:08:24 +0100

    feat: esn00-demo

---
+++ packages/esn00-demo/README.md
@@ -0,0 +1,11 @@
+# `esn00-demo`
+
+> TODO: description
+
+## Usage
+
+```
+const esn00Demo = require('esn00-demo');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/esn00-demo/src/main.js
@@ -0,0 +1,5 @@
+const handleRunClick = () => {
+  console.log('run clicked')
+}
+
+document.getElementById('run').addEventListener('click', handleRunClick)
